f6c590370b4e38956ec8dcb80a0626a1309f72d0
olenagalitska <32409783+olenagalitska@users.noreply.github.com>
2022-03-18 14:55:29 +0100

    Clear requirements

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,7 @@
 from flask import Flask
 from app.config import Config
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 from app import routes
-
-db = SQLAlchemy(app)
--- app/config.py
+++ app/config.py
@@ -3,8 +3,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'djfbviehbrkvebdjsuf'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://lena:123@localhost:5432/mana'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MONGO_URI = ""
 
     DEBUG = True
     TESTING = False
