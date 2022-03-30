0994a3a2319735b5a2b4311ff3293bbe978c4c99
olenagalitska <32409783+olenagalitska@users.noreply.github.com>
2022-03-30 14:20:36 +0200

    Add admin pages

--- app/routes/webapp.py
+++ app/routes/webapp.py
@@ -14,16 +14,36 @@ def search(name: str):
     results = Configuration.objects(name=name)
     return render_template('search.html', name=name, results=results)
 
+
 @webapp_bp.route('/configurations')
 def configurations():
     configurations = Configuration.objects()
     return render_template('configurations.html', configurations=configurations)
+
 
 @webapp_bp.route('/configurations/<name>')
 def configuration(name: str):
     config = Configuration.objects(name=name).first()
     return render_template('configuration.html', config=config)
 
+
 @webapp_bp.route('/configurations/new')
 def new_configuration():
     return render_template('new_configuration.html')
+
+
+@webapp_bp.route('/admin/configurations')
+def admin_configurations():
+    configs = Configuration.objects(showOnAdmin=True)
+    return render_template('admin.html', config=True, configurations=configs)
+
+
+@webapp_bp.route('/admin/parameters')
+def admin_parameters():
+    params = PInfo.objects()
+    return render_template('admin.html', config=False, parameters=params)
+
+
+@webapp_bp.route('/admin/parameters/new')
+def new_parameter():
+    return render_template('new_parameter.html')
