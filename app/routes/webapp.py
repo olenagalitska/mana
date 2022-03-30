28c674dc5804fbf9c989e299bb4c740f0f773d74
olenagalitska <32409783+olenagalitska@users.noreply.github.com>
2022-03-30 13:11:04 +0200

    Add wireframe templates

--- app/routes/webapp.py
+++ app/routes/webapp.py
@@ -8,3 +8,22 @@ webapp_bp = Blueprint('webapp', __name__)
 def home():
     return render_template('index.html')
 
+
+@webapp_bp.route('/search/<name>')
+def search(name: str):
+    results = Configuration.objects(name=name)
+    return render_template('search.html', name=name, results=results)
+
+@webapp_bp.route('/configurations')
+def configurations():
+    configurations = Configuration.objects()
+    return render_template('configurations.html', configurations=configurations)
+
+@webapp_bp.route('/configurations/<name>')
+def configuration(name: str):
+    config = Configuration.objects(name=name).first()
+    return render_template('configuration.html', config=config)
+
+@webapp_bp.route('/configurations/new')
+def new_configuration():
+    return render_template('new_configuration.html')
