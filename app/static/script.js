6de148d6eb4b3a97560d843bef8992b21f8621f4
olenagalitska <32409783+olenagalitska@users.noreply.github.com>
2022-04-24 19:09:20 +0200

    Add PInfo creation

--- app/static/script.js
+++ app/static/script.js
@@ -1,18 +1,33 @@
-function httpGet(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
+function httpGet(url) {
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", url, false); // false for synchronous request
+    xmlHttp.send(null);
     return xmlHttp.responseText;
 }
 
-function signIn(){
-    var password = window.prompt("Admin Password");
+function httpPost(url, body) {
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("POST", url);
+    xmlHttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlHttp.send(JSON.stringify(body));
+    return xmlHttp.responseText;
+}
+
+function signIn() {
+    let password = window.prompt("Admin Password");
     httpGet("/signin/" + password)
     location.reload()
 }
 
-function signOut(){
+function signOut() {
     httpGet("/signout")
     location.reload()
+}
+
+function addPinfo(pname, pinfo) {
+    let newPInfo = {
+        name: pname,
+        info: pinfo
+    }
+    httpPost("/api/pinfos", newPInfo)
 }
